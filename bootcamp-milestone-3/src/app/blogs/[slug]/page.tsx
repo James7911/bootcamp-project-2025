d7f11d2af67abf84360daa1e20f8824664add1e7
James <smiledog781@gmail.com>
2025-11-09 12:00:05 -0800

    d

--- bootcamp-milestone-3/src/app/blogs/[slug]/page.tsx
+++ bootcamp-milestone-3/src/app/blogs/[slug]/page.tsx
@@ -3,11 +3,7 @@ import Image from "next/image";
 import connectDB from "@/database/db";
 import { Blog } from "@/database/blogSchema";
 
-interface Props {
-  params: { slug: string };
-}
-
-export default async function BlogPost({ params }: Props) {
+export default async function BlogPost({ params }: { params: { slug: string } }) {
   await connectDB();
 
   const { slug } = params;
