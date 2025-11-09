b534d59bb53e8edb92732e8a9a9ff01bb0ba112a
James <smiledog781@gmail.com>
2025-11-09 11:40:50 -0800

    m3

--- bootcamp-milestone-3/src/app/api/Blogs/[slug]/route.ts
+++ bootcamp-milestone-3/src/app/api/Blogs/[slug]/route.ts
@@ -4,19 +4,16 @@ import { Blog } from "@/database/blogSchema";
 
 export async function GET(
   req: NextRequest,
-  context: { params: { slug: string } }
+  { params }: { params: { slug: string } } 
 ) {
-  
   await connectDB();
 
-  const { slug } = context.params;
+  const { slug } = params;
 
   try {
-    
     const blog = await Blog.findOne({ slug }).orFail();
     return NextResponse.json(blog);
   } catch (_err) {
-   
-    return NextResponse.json("Blog not found.", { status: 404 });
+    return NextResponse.json({ message: "Blog not found." }, { status: 404 });
   }
 }
